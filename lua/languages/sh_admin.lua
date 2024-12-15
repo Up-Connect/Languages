4bd5e155f6bc3cc5fa571569727efae665cef83b
Christopher <59143309+ChristopherQ-byte@users.noreply.github.com>
2024-12-15 14:35:32 +0100

    lang file Admin

---
+++ lua/languages/sh_admin.lua
@@ -0,0 +1,12 @@
+Languages.RegisterSentences("admin", "de", {
+	["prefix"] = "Admin",
+	["changedPermissions"] = "Du hast die Rechte von {{ name }} geändert",
+	["noPermissions"] = "Dazu hast du keine Rechte ({{ permissions }})",
+	["createGroup"] = "Du hast eine Gruppe mit dem Namen \"{{ name }}\" erstellt.",
+	["deleteGroup"] = "Du hast die Gruppe \"{{ name }}\" entfernt.",
+
+	//FTU
+	["banMessage"] = "\n-- Du wurdest gebannt --\nEinen Entbannungsantrag kannst du auf https://up-connect.eu schreiben.\n.............\nBan-Infos\nAdmin: {{ adminName }}({{ adminSteam }})\nErstellt: {{ startDate }}\nEndet: {{ endDate }}\nGrund: {{ reason }}\n............",
+	["familyShareBan"] = "\n-- Family-Sharing Ban ({{ target }}) --\n.............\nBan-Infos\nAdmin: {{ adminName }}({{ adminSteam }})\nErstellt: {{ startDate }}\nEndet: {{ endDate }}\nGrund: {{ reason }}\n............",
+
+})
