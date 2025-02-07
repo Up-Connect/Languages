1b4caaec758acf9c604d45884bbf6ed4829601e3
Christopher <59143309+ChristopherQ-byte@users.noreply.github.com>
2025-02-07 22:27:09 +0100

    add caracter lang

---
+++ lua/languages/sh_character.lua
@@ -0,0 +1,9 @@
+Languages.RegisterSentences("character", "de", {
+	["prefix"] = "Charakter",
+	["inPrison"] = "Du bist im Gefängnis!",
+	["unconscious"] = "Du bist bewustlos!",
+	["alreadyChangedChar"] = "Du hast deinen Charakter erst vor kurzem gewächselt!",
+	["errorChangeJob"] = "Es gab einen Fehler! Versuche einene anderen Job",
+	["nameInUse"] = "Der Name ist bereis vergeben!",
+	
+})
