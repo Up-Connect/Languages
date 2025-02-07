2861c177ddc7d6a350c051e66ca959fe26518fe4
Christopher <59143309+ChristopherQ-byte@users.noreply.github.com>
2025-02-07 22:28:09 +0100

    change admin lang

--- lua/languages/sh_admin.lua
+++ lua/languages/sh_admin.lua
@@ -8,5 +8,5 @@ Languages.RegisterSentences("admin", "de", {
 	//FTU
 	["banMessage"] = "\n-- Du wurdest gebannt --\nEinen Entbannungsantrag kannst du auf https://up-connect.eu schreiben.\n.............\nBan-Infos\nAdmin: {{ adminName }}({{ adminSteam }})\nErstellt: {{ startDate }}\nEndet: {{ endDate }}\nGrund: {{ reason }}\n............",
 	["familyShareBan"] = "\n-- Family-Sharing Ban ({{ target }}) --\n.............\nBan-Infos\nAdmin: {{ adminName }}({{ adminSteam }})\nErstellt: {{ startDate }}\nEndet: {{ endDate }}\nGrund: {{ reason }}\n............",
-
+	
 })
